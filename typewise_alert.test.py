12a9a4a69fe41363e98a4a1d5b1c989de8a45b55
Ravi Kudumu <ravikudumu@gmail.com>
2021-04-06 16:43:36 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -3,10 +3,16 @@ import typewise_alert
 
 
 class TypewiseTest(unittest.TestCase):
-    def test_check_and_alert(self):
-        self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 40) == 'TOO_HIGH')
-        self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'HI_ACTIVE_COOLING', -10) == 'TOO_LOW')
-        self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 4) == 'NORMAL')
-        
+    def test_check_and_alert_passive_cooling(self):
+        self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 40)[0] == 'TOO_HIGH')
+        self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 40)[1] == 'Controller')
+
+    def test_check_and_alert_hi_active_cooling(self):
+        self.assertTrue(typewise_alert.check_and_alert('TO_CONSOLE', 'HI_ACTIVE_COOLING', -10)[0] == 'TOO_LOW')
+        self.assertTrue(typewise_alert.check_and_alert('TO_CONSOLE', 'HI_ACTIVE_COOLING', -10)[1] == 'Console')
+
+    def test_check_and_alert_med_active_cooling(self):
+        self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 4)[0] == 'NORMAL')
+        self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 4)[1] == 'Email')
 if __name__ == '__main__':
   unittest.main()
